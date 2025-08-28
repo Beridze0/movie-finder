c406f332690beaa6887dadb09e7f4b03b72e2ec3
Beridze0 <beridzedavit904@gmail.com>
2025-08-28 18:38:04 +0400

    improved

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,17 @@ import Categories from "../ui/Categories";
 import MovieCard from "../ui/MovieCard";
 import ContinueWatchingItem from "../ui/ContinueWatchingItem";
 
+const placeholder = {
+  title: "The Incredibles",
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book`,
+  rating: 2,
+};
+
+const { title, description, rating } = placeholder;
+
 export default function HomePage() {
   return (
     <div className="w-full">
@@ -13,7 +24,11 @@ export default function HomePage() {
       {/* Continue Watching */}
       <h1>Continue Watching</h1>
 
-      <ContinueWatchingItem />
+      <ContinueWatchingItem
+        title={title}
+        description={description}
+        rating={rating}
+      />
     </div>
   );
 }
--- src/ui/ContinueWatchingItem.jsx
+++ src/ui/ContinueWatchingItem.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import panda from "../assets/panda.jpg";
 import { BiStar } from "react-icons/bi";
 
-const txt = `Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book`;
-
-export default function ContinueWatchingItem() {
+export default function ContinueWatchingItem({ title, description, rating }) {
   return (
     <div className="flex overflow-x-auto">
       <div className="flex h-[150px] w-[270px] gap-2 border">
         <img src={panda} className="w-[190px] object-fill" />
         <div>
-          <h1 className="text-md font-semibold">The Incredibles</h1>
+          <h1 className="text-md font-semibold">{title}</h1>
           <p className="text-xs">
-            {txt.split(" ").slice(0, 34).join(" ") + "..."}
+            {description.split(" ").slice(0, 34).join(" ") + "..."}
           </p>
           <div className="flex items-center gap-0.5">
-            <BiStar />
-            <BiStar />
-            <BiStar />
+            {[...Array(rating)].map((_, i) => (
+              <BiStar key={i} />
+            ))}
           </div>
         </div>
       </div>
